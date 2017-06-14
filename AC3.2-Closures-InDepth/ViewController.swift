2f7f2002b7473ba678889c5a21a8c6d50abc9b58
Louis Tur <louis.tur@gmail.com>
2017-06-14 17:14:11 -0400

    returns viewcontroller to template, rdy for lesson

--- AC3.2-Closures-InDepth/ViewController.swift
+++ AC3.2-Closures-InDepth/ViewController.swift
@@ -17,48 +17,8 @@ class ViewController: UIViewController {
   }
   
   override func viewDidAppear(_ animated: Bool) {
-//    print(self.longRunningTask())
-//    print("Done, ", self.longAdditionTask())
+    super.viewDidAppear(animated)
     
-    self.longAdditionTask { (result: Int) in
-      print("Done, ", result)
-    }
-    self.view.backgroundColor = .red
-  }
-
-  func longRunningTask() -> String {
-    
-    DispatchQueue.global().async {
-      for i in 0...250000{
-        print("Count: \(i)")
-      }
-    }
-    
-    return "ALL DONE!"
-  }
-  
-  func longAdditionTask() -> Int {
-    print("Starting Addition")
-    var result = 0
-    
-    DispatchQueue.global().async {
-      result = Array(0...10000000).reduce(0, +)
-    }
-    
-    return result
-  }
-  
-  // 1.
-  func longAdditionTask(callback: @escaping (Int)->Void) {
-    print("Starting Addition")
-    var result = 0
-    
-    DispatchQueue.global().async {
-      result = Array(0...10000000).reduce(0, +)
-      
-      // 2.
-      callback(result)
-    }
   }
   
 }
